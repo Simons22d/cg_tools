4a97d523490c5c6bcacc6be7bb29564c4352edd9
simons22d <denniskiruku@gmail.com>
2020-11-25 11:22:19 +0300

    branch unique

--- tools/models/models.py
+++ tools/models/models.py
@@ -72,7 +72,7 @@ class CategorySchema(ma.Schema):
 
 
 class Branch(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True,unique=True)
     name = db.Column(db.String(255), nullable=False, unique=True)
 
     def __init__(self, name):
