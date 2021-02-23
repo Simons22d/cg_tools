bb4a2566d424488adfa8af3feddf5a6d9ff0c32a
Denis Kiruku <denniskiruku@gmail.com>
2021-02-23 14:56:59 +0300

    pathes

--- tools/utils/utils.py
+++ tools/utils/utils.py
@@ -505,6 +505,7 @@ def send_mail(_to, subject, body,attachment=""):
     _from = "itsupport@cargen.com"
     msg = Message(subject, sender="itsupport@cargen.com", recipients=[_to], html=body)
     import os
+    os.chdir("/home/dev/cg_tools/")
     if attachment:
         with app.open_resource(f"{attachment}") as fp:
             msg.attach(f"{attachment}", "application/pdf", fp.read())
