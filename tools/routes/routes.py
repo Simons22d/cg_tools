5ca42257b1780207dcbfc5d956e65da73f12092d
Denis Kiruku <denniskiruku@gmail.com>
2021-02-23 11:00:07 +0300

    pasths

--- tools/routes/routes.py
+++ tools/routes/routes.py
@@ -125,7 +125,7 @@ def department_inform():
     import os
     os.chdir(os.path.join("/","home","dev","cg_tools","tools"))
     subprocess.run(['xvfb-run', 'wkhtmltopdf', 'http://192.168.12.200:9000/daily/report', 'branch_report.pdf'])
-    os.chdir(os.path.join(("/","home", "dev", "cg_tools"))
+    os.chdir(os.path.join(("/","home", "dev", "cg_tools")))
     # send email with attachments
     send_mail("denniskiruku@gmail.com", "daily branch report", email_report_body(image_()), attachment="branch_report.pdf")
     return jsonify({}), 200
