300166eb4d4890ac3bb98a588de8ed3e1818ebe6
Denis Kiruku <denniskiruku@gmail.com>
2021-02-24 12:13:29 +0300

    dd

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import eventlet.wsgi
 
 port = 9000
 if __name__ == '__main__':
-    app.run(port=port, host="0.0.0.0",debug=True)
+    # app.run(port=port, host="0.0.0.0",debug=True)
     eventlet.wsgi.server(eventlet.listen(("", port)), app)
